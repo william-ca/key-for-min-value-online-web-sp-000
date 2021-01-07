b6a703861b368903a40b1a5cc308090f3b57e9d2
Will Andre <wandre94@gmail.com>
2021-01-07 16:37:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
-  
+
   name_hash.each do |key, value|
     if min_value == nil
       min_key = key
@@ -12,7 +12,7 @@ def key_for_min_value(name_hash)
     elsif value < min_value
       min_key = key
       min_value = value
-    end    
+    end
   end
   min_key
 end
